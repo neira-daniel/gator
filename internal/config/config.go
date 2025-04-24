8948ec8c7f5ba2c5fa48f94a8564abfd508f1fac
Daniel Neira <keep.up.the.good.work@outlook.com>
2025-04-23 23:00:56 -0400

    feature: add login command
    This commit contains the result of completing Chapter 1 of boot.dev's
    "Build a Blog Aggregator in Go".

---
+++ internal/config/config.go
@@ -0,0 +1,81 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	DbUrl           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func getConfigFilePath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("couldn't get $HOME directory: %w", err)
+	}
+
+	path := filepath.Join(home, configFileName)
+	return path, nil
+}
+
+func write(cfg *Config) error {
+	data, err := cfg.FormatPrettyJSON()
+	// data, err := json.Marshal(cfg)
+	if err != nil {
+		return fmt.Errorf("couldn't marshal JSON configuration file: %w", err)
+	}
+
+	path, err := getConfigFilePath()
+	if err != nil {
+		return fmt.Errorf("couldn't write configuration file: %w", err)
+	}
+
+	err = os.WriteFile(path, data, 0644)
+	if err != nil {
+		return fmt.Errorf("couldn't write configuration file: %w", err)
+	}
+
+	return nil
+}
+
+func Read() (Config, error) {
+	path, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, fmt.Errorf("couldn't load '%v' from disk: %w", path, err)
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return Config{}, fmt.Errorf("couldn't load '%v' from disk: %w", path, err)
+	}
+
+	var gatorConfig Config
+	if err := json.Unmarshal(data, &gatorConfig); err != nil {
+		return Config{}, fmt.Errorf("couldn't unmarshal the contents of '%v': %w", path, err)
+	}
+
+	return gatorConfig, nil
+}
+
+func (c *Config) SetUser(user string) error {
+	c.CurrentUserName = user
+	err := write(c)
+	if err != nil {
+		return fmt.Errorf("couldn't write configuration file to disk after setting user: %w", err)
+	}
+	return nil
+}
+
+func (c *Config) FormatPrettyJSON() ([]byte, error) {
+	jsonData, err := json.MarshalIndent(c, "", "  ")
+	if err != nil {
+		return nil, fmt.Errorf("couldn't marshal JSON configuration file while prettyfying: %w", err)
+	}
+	return jsonData, nil
+}
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"gator/internal/config"
+	"log"
+	"os"
+)
+
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name      string
+	arguments []string
+}
+
+type commands struct {
+	handler map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handler[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	err := c.handler[cmd.name](s, cmd)
+	return err
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.arguments) == 0 {
+		return errors.New("didn't pass the user name to the 'login' command")
+	}
+	if len(cmd.arguments) > 1 {
+		return errors.New("too many arguments for the 'login' command")
+	}
+
+	userName := cmd.arguments[0]
+
+	if err := s.cfg.SetUser(userName); err != nil {
+		return err
+	}
+
+	fmt.Printf("username has been set to %v\n", userName)
+
+	return nil
+
+}
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatal(fmt.Errorf("couldn't read original configuration file from disk: %w", err))
+	}
+
+	s := &state{
+		cfg: &cfg,
+	}
+
+	c := commands{
+		handler: make(map[string]func(*state, command) error),
+	}
+	c.register("login", handlerLogin)
+
+	cliArgs := os.Args
+	if len(cliArgs) < 3 {
+		if len(cliArgs) < 2 {
+			log.Fatal("no command given")
+		} else {
+			log.Fatal("no argument given")
+
+		}
+	}
+
+	cmd := command{
+		name:      cliArgs[1],
+		arguments: cliArgs[2:],
+	}
+	if err := c.run(s, cmd); err != nil {
+		log.Fatal(fmt.Errorf("failed to run command '%v': %w", cmd.name, err))
+	}
+
+	// prettyCfg, err := cfg.FormatPrettyJSON()
+	// if err != nil {
+	// 	log.Fatal("couldn't prettyfy original JSON configuration file: %w", err)
+	// }
+	// fmt.Printf("original configuration file:\n%v\n", string(prettyCfg))
+
+	// userName := "test"
+
+	// err = cfg.SetUser(userName)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// cfg, err = config.Read()
+	// if err != nil {
+	// 	log.Fatal(fmt.Errorf("couldn't read modified configuration file from disk: %w", err))
+	// }
+
+	// prettyCfg, err = cfg.FormatPrettyJSON()
+	// if err != nil {
+	// 	log.Fatal("couldn't prettyfy modified JSON configuration file: %w", err)
+	// }
+	// fmt.Printf("modified configuration file:\n%v\n", string(prettyCfg))
+
+}
