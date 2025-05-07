53df07e9c351cc757a0f35eb0cebf37c1b5d1140
Daniel Neira <keep.up.the.good.work@outlook.com>
2025-05-07 00:30:04 -0400

    feat(db): add query to store feed data in DB

--- internal/database/feeds.sql.go
+++ internal/database/feeds.sql.go
@@ -55,6 +55,70 @@ func (q *Queries) CreateFeed(ctx context.Context, arg CreateFeedParams) (Feed, e
 	return i, err
 }
 
+const createFeedFollow = `-- name: CreateFeedFollow :one
+WITH feed_record AS (
+    INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING id, created_at, updated_at, user_id, feed_id
+)
+SELECT feed_record.id, feed_record.created_at, feed_record.updated_at, feed_record.user_id, feed_record.feed_id, users.name AS user_name, feeds.name AS feed_name
+FROM feed_record
+INNER JOIN users ON feed_record.user_id = users.id
+INNER JOIN feeds ON feed_record.feed_id = feeds.id
+`
+
+type CreateFeedFollowParams struct {
+	ID        uuid.UUID
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	UserID    uuid.UUID
+	FeedID    uuid.UUID
+}
+
+type CreateFeedFollowRow struct {
+	ID        uuid.UUID
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	UserID    uuid.UUID
+	FeedID    uuid.UUID
+	UserName  string
+	FeedName  string
+}
+
+func (q *Queries) CreateFeedFollow(ctx context.Context, arg CreateFeedFollowParams) (CreateFeedFollowRow, error) {
+	row := q.db.QueryRowContext(ctx, createFeedFollow,
+		arg.ID,
+		arg.CreatedAt,
+		arg.UpdatedAt,
+		arg.UserID,
+		arg.FeedID,
+	)
+	var i CreateFeedFollowRow
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.UserID,
+		&i.FeedID,
+		&i.UserName,
+		&i.FeedName,
+	)
+	return i, err
+}
+
+const getFeedIdByURL = `-- name: GetFeedIdByURL :one
+SELECT id
+FROM feeds
+WHERE url = $1
+`
+
+func (q *Queries) GetFeedIdByURL(ctx context.Context, url string) (uuid.UUID, error) {
+	row := q.db.QueryRowContext(ctx, getFeedIdByURL, url)
+	var id uuid.UUID
+	err := row.Scan(&id)
+	return id, err
+}
+
 const getFeeds = `-- name: GetFeeds :many
 SELECT feeds.name AS feed_name, feeds.url AS feed_url, users.name AS user_name
 FROM feeds
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -14,3 +14,19 @@ RETURNING *;
 SELECT feeds.name AS feed_name, feeds.url AS feed_url, users.name AS user_name
 FROM feeds
 INNER JOIN users ON feeds.user_id = users.id;
+
+-- name: GetFeedIdByURL :one
+SELECT id
+FROM feeds
+WHERE url = $1;
+
+-- name: CreateFeedFollow :one
+WITH feed_record AS (
+    INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *
+)
+SELECT feed_record.id, feed_record.created_at, feed_record.updated_at, feed_record.user_id, feed_record.feed_id, users.name AS user_name, feeds.name AS feed_name
+FROM feed_record
+INNER JOIN users ON feed_record.user_id = users.id
+INNER JOIN feeds ON feed_record.feed_id = feeds.id;
