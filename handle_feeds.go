01550e6758661a86553184c502df9fd007e97499
Daniel Neira <keep.up.the.good.work@outlook.com>
2025-05-08 18:05:59 -0400

    refactor(cmd): move feed-related code to new file

---
+++ handle_feeds.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"gator/internal/database"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func handlerAgg(s *state, cmd command) error {
+	if len(cmd.arguments) != 0 {
+		return errors.New("'agg' doesn't take arguments")
+	}
+
+	ctx := context.Background()
+	url := "https://www.wagslane.dev/index.xml"
+	xmlData, err := fetchFeed(ctx, url)
+	if err != nil {
+		return fmt.Errorf("fetching feed in %v: %w", url, err)
+	}
+
+	fmt.Printf("%v\n", xmlData)
+
+	return nil
+}
+
+func handlerAddFeed(s *state, cmd command, userData database.User) error {
+	if len(cmd.arguments) != 2 {
+		return fmt.Errorf("usage: %v <feed name> <feed URL>", cmd.name)
+	}
+
+	ctx := context.Background()
+	timestamp := time.Now().UTC()
+	feedParams := database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: timestamp,
+		UpdatedAt: timestamp,
+		Name:      cmd.arguments[0],
+		Url:       cmd.arguments[1],
+		UserID:    userData.ID,
+	}
+
+	_, err := s.db.CreateFeed(ctx, feedParams)
+	if err != nil {
+		return fmt.Errorf("storing feed data to the database: %w", err)
+	}
+
+	if err := handlerFollow(s, command{name: cmd.name, arguments: []string{cmd.arguments[1]}}, userData); err != nil {
+		return fmt.Errorf("following feed after adding it: %w", err)
+	}
+
+	return nil
+}
+
+func handlerFeeds(s *state, cmd command) error {
+	if len(cmd.arguments) != 0 {
+		return fmt.Errorf("%q doesn't take arguments", cmd.name)
+	}
+
+	ctx := context.Background()
+	feeds, err := s.db.GetFeeds(ctx)
+	if err != nil {
+		return fmt.Errorf("getting feed info. from db: %w", err)
+	}
+
+	for _, feed := range feeds {
+		fmt.Printf("%q\t%v\t%v\n", feed.FeedName, feed.FeedUrl, feed.UserName)
+	}
+
+	return nil
+}
+
+func handlerFollow(s *state, cmd command, userData database.User) error {
+	if len(cmd.arguments) != 1 {
+		return fmt.Errorf("usage: %v <url>", cmd.name)
+	}
+
+	ctx := context.Background()
+	feedID, err := s.db.GetFeedIdByURL(ctx, cmd.arguments[0])
+	if err != nil {
+		return fmt.Errorf("getting feed data: %w", err)
+	}
+
+	timestamp := time.Now().UTC()
+	feedFollowParams := database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: timestamp,
+		UpdatedAt: timestamp,
+		UserID:    userData.ID,
+		FeedID:    feedID,
+	}
+
+	feedFollowData, err := s.db.CreateFeedFollow(ctx, feedFollowParams)
+	if err != nil {
+		return fmt.Errorf("creating feed follow record: %w", err)
+	}
+
+	fmt.Printf("%v", feedFollowData)
+
+	return nil
+}
+
+func handlerFollowing(s *state, cmd command, userData database.User) error {
+	if len(cmd.arguments) != 0 {
+		return fmt.Errorf("%q doesn't take arguments", cmd.name)
+	}
+
+	ctx := context.Background()
+	feeds, err := s.db.GetFeedFollowsForUser(ctx, userData.ID)
+	if err != nil {
+		return fmt.Errorf("getting feed follows: %w", err)
+	}
+
+	fmt.Printf("%+v\n", feeds)
+
+	return nil
+}
