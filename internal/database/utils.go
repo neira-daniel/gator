def871afafe74b03e33892e6f717071e46bed0e4
Daniel Neira <keep.up.the.good.work@outlook.com>
2025-05-08 17:57:52 -0400

    chore: improve readability of implemented `String()` methods

--- internal/database/utils.go
+++ internal/database/utils.go
@@ -3,11 +3,21 @@ package database
 import "fmt"
 
 func (u User) String() string {
-	return fmt.Sprintf("* ID        : %v\n* CreatedAt : %v\n* UpdatedAt : %v\n* Name      : %v", u.ID, u.CreatedAt, u.UpdatedAt, u.Name)
+	return fmt.Sprintf(`* ID        : %v
+* CreatedAt : %v
+* UpdatedAt : %v
+* Name      : %v
+`, u.ID, u.CreatedAt, u.UpdatedAt, u.Name)
 }
 
 func (f Feed) String() string {
-	return fmt.Sprintf("* ID        : %v\n* CreatedAt : %v\n* UpdatedAt : %v\n* Name      : %v\n* Url       : %v\n* UserID    : %v\n", f.ID, f.CreatedAt, f.UpdatedAt, f.Name, f.Url, f.UserID)
+	return fmt.Sprintf(`* ID        : %v
+* CreatedAt : %v
+* UpdatedAt : %v
+* Name      : %v
+* Url       : %v
+* UserID    : %v
+`, f.ID, f.CreatedAt, f.UpdatedAt, f.Name, f.Url, f.UserID)
 }
 
 func (f CreateFeedFollowRow) String() string {
