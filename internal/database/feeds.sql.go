365e6ae33f779e2de501e6b5f35a46d506ba0f70
Daniel Neira <keep.up.the.good.work@outlook.com>
2025-05-08 23:20:08 -0400

    feat(db): add query to unfollow feeds

--- internal/database/feeds.sql.go
+++ internal/database/feeds.sql.go
@@ -190,3 +190,18 @@ func (q *Queries) GetFeeds(ctx context.Context) ([]GetFeedsRow, error) {
 	}
 	return items, nil
 }
+
+const unfollowFeed = `-- name: UnfollowFeed :exec
+DELETE FROM feed_follows
+WHERE user_id = $1 AND feed_id = $2
+`
+
+type UnfollowFeedParams struct {
+	UserID uuid.UUID
+	FeedID uuid.UUID
+}
+
+func (q *Queries) UnfollowFeed(ctx context.Context, arg UnfollowFeedParams) error {
+	_, err := q.db.ExecContext(ctx, unfollowFeed, arg.UserID, arg.FeedID)
+	return err
+}
